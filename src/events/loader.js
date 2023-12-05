fc1f2b8a88e7c4fec6bd657cdaa9b43e87b58797
Su386 <83100266+Su386yt@users.noreply.github.com>
2023-12-05 09:53:34 -0800

    Added test server to the guilds id and fixed issue with loader not loading if bot was not on the server

--- src/events/loader.js
+++ src/events/loader.js
@@ -43,7 +43,8 @@ module.exports = async (client) => {
                 /*
                  * Creating them globally is not best, as it takes a really long time for them to update
                 */
-                client.guilds.cache.get(id).commands.create(data);
+                client.guilds.cache.get(id)?.commands.create(data);
+                console.log(`Added commands to server ${id}`)
             }
         }
 
