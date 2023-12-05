333f8481d88299fcd6ff6fba426729407aefef0a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2023-12-05 15:02:16 +0100

    CleanUp on onCommandExec

--- src/events/onCommandExec.js
+++ src/events/onCommandExec.js
@@ -1,19 +1,5 @@
 module.exports = async (client) => {
     client.on("interactionCreate", async (interaction) => {
-        if (interaction.isAutocomplete()) {
-            const cmd = client.commands.get(interaction.commandName)
-
-            if (!cmd) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-            try {
-                await cmd.autocomplete(interaction, client)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-
         if (!interaction.isCommand()) return;
         const cmd = client.commands.get(interaction.commandName) ?? null;
 
@@ -24,5 +10,23 @@ module.exports = async (client) => {
 
         let options = interaction.options._hoistedOptions;
         cmd.run(client, interaction, options);
+    })
+
+    client.on("interactionCreate", async (interaction) => {
+        if (!interaction.isAutocomplete()) return;
+        const cmd = client.commands.get(interaction.commandName)
+
+        if (!cmd) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        if (!cmd.autocomplete) return;
+
+        try {
+            await cmd.autocomplete(interaction, client)
+        } catch (error) {
+            console.error(error)
+        }
     })
 }
