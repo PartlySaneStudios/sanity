d451fdabf74560d8171f41ffdca08091daca11e7
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2023-12-19 23:49:09 +0100

    Add onChatMessage event handler

---
+++ src/events/onChatMessage.js
@@ -0,0 +1,16 @@
+//
+// Written by J10a1n15.
+// See LICENSE for copyright and license notices.
+//
+
+
+const { Events } = require('discord.js');
+
+module.exports = async (client) => {
+    client.on(Events.MessageCreate, async (message) => {
+        if (message.author.bot) return;
+        if (message.channel.type === 'DM') return;
+
+
+    });
+}
