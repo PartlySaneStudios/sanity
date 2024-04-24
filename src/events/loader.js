3868c89298e9bcb1e6700e4473a3d42464b00d86
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-04-23 23:38:38 -0700

    This is getting old now

--- src/events/loader.js
+++ src/events/loader.js
@@ -8,67 +8,67 @@ const { Collection } = require('discord.js')
 const fs = require("fs");
 
 module.exports = async (client) => {
-    client.commands = new Collection();
+  client.commands = new Collection();
 
-    fs.readdir("src/events", (err, files) => {
-        if (err || files.length <= 0) return console.error(err || "No events found.");
+  fs.readdir("src/events", (err, files) => {
+    if (err || files.length <= 0) return console.error(err || "No events found.");
 
-        files.forEach((file) => (file.endsWith(".js") && file !== "loader.js") && require(`./${file}`)(client));
-    });
+    files.forEach((file) => (file.endsWith(".js") && file !== "loader.js") && require(`./${file}`)(client));
+  });
 
-    require("../features/automatic/serverStatus")(client);
-    require("../features/automatic/setBotStatus")(client);
-    require("../features/automatic/dailyFunFact")(client);
+  require("../features/automatic/serverStatus")(client);
+  require("../features/automatic/setBotStatus")(client);
+  require("../features/automatic/dailyFunFact")(client);
 
-    /* 
-     * Load commands
-    */
-    // Read all files in the commands folder
-    loadAllFilesInFolder("src/features/commands", client)
+  /* 
+   * Load commands
+  */
+  // Read all files in the commands folder
+  loadAllFilesInFolder("src/features/commands", client)
 
-    console.log("\x1b[42m%s\x1b[0m", "Ready.");
+  console.log("\x1b[42m%s\x1b[0m", "Ready.");
 };
 
 // Loads all commands recursively
 function loadAllFilesInFolder(path, client) {
-    // Read all files in the commands folder
-    fs.readdir(path, (err, files) => {
-        let commandsAdded = 0
-        if (err) return console.error(err);
-        if (files.length <= 0) return console.log(`No slash commands found in ${path}.`);
+  // Read all files in the commands folder
+  fs.readdir(path, (err, files) => {
+    let commandsAdded = 0
+    if (err) return console.error(err);
+    if (files.length <= 0) return console.log(`No slash commands found in ${path}.`);
 
-        for (let fileName of files) {
-            const fullFilePath = `${path}/${fileName}`
-            // If the file is a folder, run itself
-            if (fs.lstatSync(fullFilePath).isDirectory()) {
-                loadAllFilesInFolder(`${fullFilePath}`, client)
-                continue;
-            }
-            // If the file is not a .js file, pass
-            if (fileName.split(".").pop() != "js") {
-                continue
-            }
-            // Load file
-            let command = require(`../../${fullFilePath}`);
-            if ("data" in command && "run" in command) {
-                // Set a new command in the Collection
-                client.commands.set(command.data.name, command);
+    for (let fileName of files) {
+      const fullFilePath = `${path}/${fileName}`
+      // If the file is a folder, run itself
+      if (fs.lstatSync(fullFilePath).isDirectory()) {
+        loadAllFilesInFolder(`${fullFilePath}`, client)
+        continue;
+      }
+      // If the file is not a .js file, pass
+      if (fileName.split(".").pop() != "js") {
+        continue
+      }
+      // Load file
+      let command = require(`../../${fullFilePath}`);
+      if ("data" in command && "run" in command) {
+        // Set a new command in the Collection
+        client.commands.set(command.data.name, command);
 
-                // Looping through all config entries
-                for (let id of require("../config/config.json").guilds) {
-                    /*
-                     * Creating them globally is not best, as it takes a really long time for them to update
-                    */
-                    client.guilds.cache.get(id)?.commands.create(command.data.toJSON());
-                    //console.log(`Added commands from ${path} to server ${id}`)
-                }
-            } else {
-                console.log(`Missing "data" or "run" for command at ${data.name}.`)
-            }
-
-            commandsAdded++
+        // Looping through all config entries
+        for (let id of require("../config/config.json").guilds) {
+          /*
+           * Creating them globally is not best, as it takes a really long time for them to update
+          */
+          client.guilds.cache.get(id)?.commands.create(command.data.toJSON());
+          //console.log(`Added commands from ${path} to server ${id}`)
         }
+      } else {
+        console.log(`Missing "data" or "run" for command at ${data.name}.`)
+      }
 
-        console.log(`Added ${commandsAdded} command${commandsAdded != 1 ? "s" : ""} total to ${require("../config/config.json").guilds.length} server${require("../config/config.json").guilds.length != 1 ? "s" : ""}.`)
-    })
+      commandsAdded++
+    }
+
+    console.log(`Added ${commandsAdded} command${commandsAdded != 1 ? "s" : ""} total to ${require("../config/config.json").guilds.length} server${require("../config/config.json").guilds.length != 1 ? "s" : ""}.`)
+  })
 }
